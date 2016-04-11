14a48dace29a58ad8a1f26d8a9ffd91d68addd8e
Jessica Wynn <Jessicashinjo@gmail.com>
2016-04-11 15:10:42 -0500

    final changes

--- sonnet.js
+++ sonnet.js
@@ -1,30 +1,26 @@
 
-
-
-
-
-
-
 // Take the contents of the sonnet div and place it in a variable
 var sonnetOuter = document.getElementById("sonnet");
+// console.log("sonnetOuter", sonnetOuter);
+
 var sonnet = sonnetOuter.innerHTML;
-console.log(sonnet);
+// console.log("sonnet", sonnet);
 
 // Find and output the starting position of the word "orphans"
 var orphansLoc = sonnet.indexOf("orphans");
-console.log("orphans location", orphansLoc);
+// console.log("orphans location", orphansLoc);
 
 // Output the number of characters in the sonnet
 console.log("Length of sonnet", sonnet.length);
 
 // Replace the first occurance of the string "winter" with "yuletide"
 var winter = sonnet.replace("winter", "yuletide");
-console.log("replace Winter", winter);
+// console.log("replace Winter", winter);
 
 // Replace all occurances of the string "the" with "a large"
-var theUpdate = sonnet.replace(/[T|t]he /g, "a large ");
-console.log("the to large", theUpdate);
+winter += sonnet.replace(/[T|t]he /g, "a large ");
+// console.log("the to large", theUpdate);
 
 // Set the content of the sonnet div with the new string
 
-sonnetOuter.innerHTML = theUpdate;
+sonnetOuter.innerHTML = winter;
